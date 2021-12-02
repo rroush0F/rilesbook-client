1b5a8bd1a3625a3ca68a257bdc3ce34908569108
Riley Roush <riley.roush@gmail.com>
2021-12-02 10:43:14 -0500

    workkkk

--- src/helpers/environment.js
+++ src/helpers/environment.js
@@ -5,7 +5,7 @@ switch(window.location.hostname) {
         APIURL = "http://localhost:3000"
         break;
     case "rilesbookclient.herokuapp.com":
-        APIURL = "rilesbookserver.herokuapp.com"
+        APIURL = "https://rilesbookserver.herokuapp.com"
 } 
 
 export default APIURL;
